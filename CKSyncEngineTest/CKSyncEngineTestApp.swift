78c3baed9eb8b8dac171392115fbf32ba9a93a17
Ben Gottlieb <ben@standalone.com>
2023-08-06 17:56:58 -0500

    Initial Commit

---
+++ CKSyncEngineTest/CKSyncEngineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CKSyncEngineTestApp.swift
+//  CKSyncEngineTest
+//
+//  Created by Ben Gottlieb on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CKSyncEngineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CKSyncEngineTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CKSyncEngineTest
+//
+//  Created by Ben Gottlieb on 8/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
